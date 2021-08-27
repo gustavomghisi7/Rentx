5dcbd587c3950900232e5e4c5e5ab3ad95acf9e8
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2021-08-27 17:52:39 -0300

    structuring the data

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -30,21 +30,29 @@ export function Home(){
     }
 
     useEffect(() => {
+        let isMounted = true;
+        
         async function fetchCars() {
             try {
                 const response = await api.get('/cars');
-                setCars(response.data);
+                if(isMounted){
+                    setCars(response.data);
+                }
 
             } catch (error) {
                 console.log(error);
 
             } finally {
+                if(isMounted){}
                 setLoading(false);
-
             }
         }
 
         fetchCars();
+        return () => {
+            isMounted = false;
+            
+        };
     }, []);
 
     return(
